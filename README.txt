00d2842a63f0652c8a261cc32ee4b0d838bc420b
vivacityinfotech.jaipur <vivacityinfotech.jaipur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-04 17:40:21 +0000

    old 1.2 version is upload to repository
    git-svn-id: https://plugins.svn.wordpress.org/wp-facebook-auto-publish/tags/1.2@1212751 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,71 @@
+=== WP Facebook Auto Publish ===
+Contributors: vivacityinfotech.jaipur
+Donate link: http://tinyurl.com/owxtkmt
+Tags: Facebook , Facebook Post, Facebook Wall, Auto Post, Auto Plublish, Facebook Publish, Facebook Auto Post, Facebook Auto Plublish, Post on Facebook, Page on Facebook, Custom Post on Facebook, Auto Custom Post on Facebook 
+Requires at least:3.0
+Tested up to:3.9
+Stable tag: 1.2
+License: GPLv2 or later
+
+== Description ==
+
+A Simple wordpress plugin to automatically post your wordpress posts and pages on Facebook along with their featured image. You can also auto post your custom post on facebook wall. It is very easy to set up and configure.
+
+You can also customize setting for following features:
+
+* You can auto post you facebook posts, page and custom post on facebook ball.
+* All the post and page will posted on facebook along with their feature image.
+* You can auto publish the post and page on your or others facebook page
+* You can easily authicate yourself by just clicking on Authenticate button.
+* You can customize the setting for sending a default image if no image is available for your post.
+* You can also set the default image by putting the link of that image.
+
+= Rate Us / Feedback =
+
+Please take the time to let us and others know about your experiences by leaving a review, so that we can improve the plugin for you and other users.
+
+= Want More? =
+
+If You Want more functionality or some modifications, just drop us a line what you want and We will try to add or modify the plugin functions.
+
+If you like the plugin please [Donate here](http://tinyurl.com/owxtkmt). 
+
+== Installation ==
+
+
+1. Download the WP Facebook Auto Publish Plugin.
+2. Extract it in the `/wp-content/plugins/` directory
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Customize the plugin in the WP2FB Auto Post
+
+
+Visit our <a href="http://vivacityinfotech.net/blog/facebook-auto-post-wordpress-plugin/">demo page</a> for more information.
+
+
+== Frequently Asked Questions ==
+
+= How to set up WP Facebook Auto Publish plugin =
+
+In WordPress admin panel go to "WP2FB Auto Post", add the facebook app ID and secret ID and click on "Authenticate" button to authenticate yourself on facebook. Then select the post type which you want to be auto published on facebook and click "Save Changes"
+
+
+== Screenshots ==
+1. Plugin options will be found at Admin Panel Menus named as WP2FB Auto Post
+2. App ID, Secret ID, Page ID, Authtication, Post type and Default Image setting for WP Facebook Auto Publish
+3. Facebook Post auto posted by plugin
+
+== PHP Version ==
+
+PHP 5+ is supported.
+
+
+== Changelog ==
+
+= 1.1 =
+* Home URL bug is resolved.
+
+= 1.0 =
+* Stable version.
+
+Visit our <a href="http://vivacityinfotech.net/blog/facebook-auto-post-wordpress-plugin/">demo page</a> for more information.
+
---
+++ wp-fap-gui.php
@@ -0,0 +1,96 @@
+<?php
+ $url = plugin_dir_url(__FILE__);
+ $post_types = get_post_types();
+
+$id = get_option('wfap_app_id');
+$sec = get_option('wfap_sec_id');
+$page = get_option('wfap_page_id');
+//$user_tkn = get_option('wfap_user_tkn');
+$default_img = get_option('wfap_default_img');
+if($default_img == 0)
+	echo '<style type="text/css">#default_img_link {display:none;}</style>';
+$default_img_link = get_option('wfab_default_img_link');
+$post_list = array();
+$post = get_option('wfap_post_type');
+$post_list  = explode(",", $post );
+?>
+<script type="text/javascript">
+	var url = '"<?php echo $url; ?>';
+	var admin_url = '<?php echo admin_url('admin-ajax.php'); ?>';
+</script>
+<link href="<?php echo $url; ?>css/fbstyle.css" type="text/css" rel="stylesheet" />
+<script src="<?php echo $url; ?>js/fbscript.js" type="text/javascript"></script>
+<div id="wrapper">
+<span id="status_msg" class="">Setting saved</span>
+	<div class="container">
+		<form action="" method="post">
+	        <div class="fb_form">
+	           <h2>WP Facebook Auto Publish</h2>
+                <table>
+                    <tr>
+						<td>Facebook App ID<span class="create_app">To create facebook app click <a href="https://developers.facebook.com/apps/">here</a></span></td>                    
+                        <td><input type="text" name="fb_app_id" id="fb_app_id" value="<?php echo $id; ?>" /> </td>
+                    </tr>
+                    <tr>
+						<td>Facebook App Secret</td>                    
+                        <td><input type="text" name="fb_app_sec_id" id="fb_app_sec_id" value="<?php echo $sec; ?>" /> 
+                        <input type="button" id="wfap_authenticate" value=" Authenticate"  class="button-primary" /><span id="auth_gif"><img src="<?php echo $url;?>images/loading_gif.gif" /> Authenticating... </span> 
+                        </td>
+                    </tr>
+					<tr>
+						<td>Page or Profile</td>                    
+                        <td><input type="text" name="fb_page_id" id="fb_page_id" value="<?php echo $page; ?>" /> 
+                        	<span class="note">Note: You can use "me" or leave blank for your own profile ID</span>
+                        </td>
+                    </tr>
+                    <tr>
+						<td>Post Type</td>                    
+                        <td>
+                        	<div class="list_custom">
+                            <?php foreach($post_types as $pt){ 
+
+								if($pt != "attachment" && $pt != "revision" && $pt != "nav_menu_item" ) {?>                            	
+                                <span><input type="checkbox" name="post_ty[]" value="<?php echo $pt; ?>" <?php if (in_array($pt, $post_list)) echo ' checked="checked"'; ?> /><?php echo $pt; ?></span>
+                            <?php } } ?>
+                            </div>
+                        </td>
+                    </tr>
+				 <tr>
+						<td>Show Default Image If no Image available</td>                    
+                        <td>
+	                        <div>
+                                <span><input type="checkbox" name="default_img" id="default_img" value="1"  <?php if ($default_img == 1) echo ' checked="checked"'; ?> /></span>
+                                <span><input type="text" name="default_img_link" id="default_img_link" value="<?php if (empty($default_img_link)) echo 'Add the link of the default image'; else echo $default_img_link;?>" onblur="add_label('Add the link of the default image')"  /></span>
+                           </div>
+                        </td>
+                    </tr>
+               </table>
+                <input type="submit" value="Save Changes" id="save_btn" class="button-primary" /><span id="load_gif"><img src="<?php echo $url;?>images/loading_gif.gif" /> Saving Settings ... </span>
+            </div>
+        </form>
+    </div>
+    
+    <div class="sidebar">
+    
+			<h3 class="hndle"><span>Important Note</span></h3>
+			<div class="inside">
+				<p>According to the new security policy of the facebook, the App needs to be submitted and verified by facebook team.</p>
+				<p>Please follow the bellow instructions to submit an app.</p>
+				<ol>
+					<li>A logo must be provided to the App. In order to add a logo, go your facebook <strong>App->App Details->Icons</strong>.</li>
+					<li>Go to your facebook <strong>App->Status & Review->Start a Submission</strong>. Give the required "<strong>LOGIN PERMISSIONS</strong>" to the app to be used and accessed by your domain and by our plugin.
+						<p>Your app must have below permissions to use auto post feature</p>
+						<ul>
+							<li>manage_pages</li>
+							<li>publish_actions</li>
+						</ul>
+						<p>You also need to explain the use of the above permissions on facebook.</p>
+					</li>
+					<li>You must provide a long description of your app. Go to you facebook <strong>App->App Details->Long Description</strong>. This description must explain a valid reason or purpose to use this app for your domain.
+					<br/><br/> Later all the above settings will be reviewed by the facebook team with in next 7 business days.</li>
+				</ol>
+							
+			</div>
+			
+    </div>
+</div>
